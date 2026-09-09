23529f3f6173980a9d526bd3f6901513e3c5df00
izmi <s251594708@studentmail.unimap.edu.my>
2026-09-09 15:31:42 +0800

    Update MediaEquipmentRental.java

--- MediaEquipmentRental/src/mediaequipmentrental/MediaEquipmentRental.java
+++ MediaEquipmentRental/src/mediaequipmentrental/MediaEquipmentRental.java
@@ -7,10 +7,10 @@ package mediaequipmentrental;
 public class MediaEquipmentRental {
 
     private String equipmentName;
+    private String equipmentID;
     
     public static void main(String[] args) {
         // TODO code application logic here
     }
     
 }
-....
