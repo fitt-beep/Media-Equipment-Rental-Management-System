aa2ab816b19aa7c94746777caef38a9738ac74e2
fitt-beep <s251594697@studentmail.unimap.edu.my>
2026-09-13 23:17:09 +0800

    Update MediaEquipmentRental.java

--- MediaEquipmentRental/src/mediaequipmentrental/MediaEquipmentRental.java
+++ MediaEquipmentRental/src/mediaequipmentrental/MediaEquipmentRental.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package mediaequipmentrental;
 
 public class MediaEquipmentRental {
 
-    private String equipmentName;
-    private String equipmentID;
     
     public static void main(String[] args) {
         // TODO code application logic here
