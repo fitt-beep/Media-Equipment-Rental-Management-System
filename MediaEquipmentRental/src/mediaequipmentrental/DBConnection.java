38113d65d11160de3c5c86c550151f7b81b6f4a5
fitt-beep <s251594697@studentmail.unimap.edu.my>
2026-09-14 15:02:58 +0800

    Update DBConnection.java

--- MediaEquipmentRental/src/mediaequipmentrental/DBConnection.java
+++ MediaEquipmentRental/src/mediaequipmentrental/DBConnection.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package mediaequipmentrental;
 
-/**
- *
- * @author User
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
-    
+
+    private static final String URL = "jdbc:sqlite:C:/Users/User/Documents/GitHub/Media-Equipment-Rental-Management-System/database/media_equipment_rental.db";
+
+    public static Connection connect() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(URL);
+            System.out.println("Database connected successfully!");
+        } catch (SQLException e) {
+            System.out.println("Database connection failed: " + e.getMessage());
+        }
+
+        return conn;
+    }
 }
